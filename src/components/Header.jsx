6ef04738bec95d0704d64e431f6b24d369ed7566
erichudson <defideveric@gmail.com>
2023-02-10 10:22:57 -0500

    Next Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import AppShortcutIcon from '@mui/icons-material/AppShortcut';
 
 function Header() {
-    return <header>
-        <h1>Keeper</h1>
+  return (
+    <header>
+      <h1><AppShortcutIcon/>Sentry</h1>
     </header>
+  );
 }
-
 
 export default Header;
